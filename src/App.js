d266b2235b451abdcffc7e45c848d28544a960ae
Sinx Wang <cosx1999@hotmail.com>
2021-04-07 18:45:13 +0800

    login view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { Redirect, Route, Router, Switch } from 'react-router-dom'
+// import logo from './logo.svg';
+import { createBrowserHistory } from 'history';
+import LoginView from './views/LoginView';
 import './App.css';
 
+const hist = createBrowserHistory();
+
 function App() {
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#2196F3",
+      },
+      secondary: {
+        main: "#F50057",
+      },
+      error: {
+        main: "#F44336",
+      },
+    },
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +38,15 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <MuiThemeProvider theme={theme}>
+        <Router history={hist}>
+          <Switch>
+            <Route path='/login' component={LoginView} />
+            <Redirect from='/' to='/login' />
+          </Switch>
+        </Router>
+      </MuiThemeProvider>
     </div>
   );
 }
